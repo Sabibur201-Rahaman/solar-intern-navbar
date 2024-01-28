d3c742bfa85c231eb0dfa11bbe571d7a53ac0df4
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2024-01-29 00:01:46 +0530

    navbar applied using hamberg fontawesome icon with animation also

---
+++ index.js
@@ -0,0 +1,13 @@
+let navStatus = 0;
+
+function openNav() {
+  if (!navStatus) {
+    $(".maindiv").css("width", "100%");
+    $("li").css("display", "block");
+    navStatus = 1;
+  } else {
+    $(".maindiv").css("width", "0");
+    $("li").css("display", "none");
+    navStatus = 0;
+  }
+}
